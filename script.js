22daa98cc6c8308ab9c3efb843e07362ab15fd3c
Garghettis <garghettis@unochapeco.edu.br>
2024-10-17 20:05:42 -0300

    subindo index atualizado

---
+++ script.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMQtt7KP30AcHi72FHtaZtZOXIYL7DVv0",
+  authDomain: "medidoso.firebaseapp.com",
+  databaseURL: "https://medidoso-default-rtdb.firebaseio.com",
+  projectId: "medidoso",
+  storageBucket: "medidoso.appspot.com",
+  messagingSenderId: "1006734497522",
+  appId: "1:1006734497522:web:98f5911a09c35146bb626b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
